d14a0aa272cc35707f441ea24db5367341120362
Andrew Kerr <kerrand@protonmail.com>
2022-01-16 11:51:06 -0500

    ignore .vscode'

--- sandbox.cpp
+++ sandbox.cpp
@@ -9,6 +9,9 @@ int main() {
     BigInt a("1001");
     BigInt b("2");
 
+    cout << "-1 % 10: " << -1 % 10 << endl;
+    cout << "-1 mod 10: " << -1 - 10 * std::floor(-1.0 / 10.0) << endl;
+
     cout << "a - b: " << a - b << endl;
     cout << "b - a: " << b - a << endl;
     cout << "a - a: " << a - a << endl;
